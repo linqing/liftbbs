4d5777464cbfe6f8dfe5009860f3246ac7faf5cf
linqing <linqing01@gmail.com>
2011-07-19 19:54:14 +0800

    完成首页论坛显示

--- src/main/scala/liftbbs/snippet/Forums.scala
+++ src/main/scala/liftbbs/snippet/Forums.scala
@@ -6,9 +6,17 @@ import PrimitiveTypeMode._
 import org.squeryl.annotations._
 
 import liftbbs.model.BbsModel._
+
+import scala.xml._
+import net.liftweb.util.Helpers._
+
 class Forums {
   def render =
     for (f <- forums) yield {
       <h1>{ f.name }</h1>
     }
+  def list = {
+    "#forum *" #> forums.map( f => "@nameLink [href]" #> {"forumdisplay?fid=" + f.id} &
+                             "@nameLink *" #> {f.name} )
+  }
 }
